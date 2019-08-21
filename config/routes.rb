642c5f25bcd3cf9e3d89f03c7dcc173a3e36beef
Ricky Wolff <ricky@bigsouthernsoftware.com>
2019-08-21 15:29:59 -0400

    save code chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'sessions/new'
   get 'users/new'
 
   root 'static_pages#home'
@@ -16,5 +18,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 end
