66af594cc4bf4ffc280a6b32463dce9eeb358a6e
Ricky Wolff <ricky@bigsouthernsoftware.com>
2019-08-22 17:41:28 -0400

    save code chapter 14

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-
+  
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -77,6 +81,21 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id) # i.e. self.id; the '?' escapes the 'id' to avoid SQL injection
   end
   
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
 
     # Converts email to all lower-case.
